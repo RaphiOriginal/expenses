bbd84ab52d0c032035ac85c025e2b58edc414b73
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-06-02 14:48:02 +0200

    adding accessright basic model

---
+++ lib/model/accessright.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from base import Base
+
+class AccessRight(Base):
+    __tablename__ = 'accessright'
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    household_id = Column(Integer, ForeignKey('houesehold.idr'))
+
+    def __repr__(self):
+        return "<AccessRight(user_id='%s', household_id='%s')" % (self.user_id, self.household_id)
