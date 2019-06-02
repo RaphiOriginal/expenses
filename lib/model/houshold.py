c1f28f500703756ae5812f89b2cbd3afe0915e69
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-06-02 10:14:03 +0200

    adding member relation

--- lib/model/houshold.py
+++ lib/model/houshold.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
+from member import Member
 
 Base = declarative_base()
 
@@ -9,6 +10,8 @@ class Household(Base):
     id = Column(Integer, primary_key=True)
 
     name = Column(String(255))
+
+    members = relationship("Member", order_by=Member.id, back_popultes="household")
 
     def __repr__(self):
         return "<Household(name='%s')>" % (self.name)
