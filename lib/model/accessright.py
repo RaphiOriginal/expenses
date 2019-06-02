873ec112472cb08bf22b36003d1fb7b50c81229e
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-06-02 15:07:22 +0200

    fixing typo

--- lib/model/accessright.py
+++ lib/model/accessright.py
@@ -6,7 +6,7 @@ class AccessRight(Base):
     id = Column(Integer, primary_key=True)
 
     user_id = Column(Integer, ForeignKey('user.id'))
-    household_id = Column(Integer, ForeignKey('houesehold.id'))
+    household_id = Column(Integer, ForeignKey('household.id'))
 
     def __repr__(self):
         return "<AccessRight(user_id='%s', household_id='%s')" % (self.user_id, self.household_id)
