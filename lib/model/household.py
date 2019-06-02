d53d35652e96fc4dbeee4471f483c1917ec074d7
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-06-02 15:08:04 +0200

    adding relation to accessright table

--- lib/model/household.py
+++ lib/model/household.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Sequence
 from sqlalchemy.orm import relationship
 from lib.model.member import Member
+from lib.model.accessright import AccessRight
 from base import Base
 
 class Household(Base):
@@ -11,6 +12,7 @@ class Household(Base):
     name = Column(String(255))
 
     members = relationship("Member")
+    accessrights = relationship("AccessRight")
 
     def __repr__(self):
         return "<Household(name='%s')>" % (self.name)
