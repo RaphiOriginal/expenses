17ad190cdc8b3776ee0dd806548a440c71606184
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-06-02 15:02:27 +0200

    adding accessright relation to user

--- lib/model/user.py
+++ lib/model/user.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy.orm import relationship
 from base import Base
+from lib.model.accessright import AccessRight
 
 class User(Base):
     __tablename__ = 'user'
@@ -9,6 +11,8 @@ class User(Base):
     email = Column(String(255))
     salt = Column(String(255))
     password_hash = Column(String(255))
+
+    accessrights = relationship("AccessRight")
 
     def __repr__(self):
         return "<User(email='%s', salt='%s', password_hash='%s')>" % (self.email, self.salt, self.password_hash)
