bed509251b6edf665010941b9d1e7ca152125e54
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-06-04 20:49:51 +0200

    adding put and delete for household service

--- service.py
+++ service.py
@@ -98,7 +98,7 @@ class HouseholdService(object):
         user = cherrypy.session['user']
         if user is None:
             raise cherrypy.HTTPError(401, 'Unauthorized')
-        accessrights = db.query(AccessRight).filter(AccessRight.user_id == user.id).all()
+        accessrights = user.accessrights
         print("DEBUG: Accessrights to" + str(accessrights))
         if accessrights is None or len(accessrights) == 0:
             raise cherrypy.HTTPError(401, 'Unauthorized')
@@ -115,6 +115,31 @@ class HouseholdService(object):
                 return str(household)
             else:
                 raise cherrypy.HTTPError(401, 'Unauthorized')
+
+    @cherrypy.tools.accept(media='text/plain')
+    def PUT(self, id, name):
+        db = cherrypy.request.db
+        user = cherrypy.session['user']
+        if user is None:
+            raise cherrypy.HTTPError(401, 'Unauthorized')
+        legal_ids = list(map(lambda x: int(x.household_id), user.accessrights))
+        print("DEBUG: " + str(legal_ids))
+        if int(id) in legal_ids:
+            household = db.query(Household).filter(Household.id == id).one()
+            household.name = name
+        else:
+            raise cherrypy.HTTPError(401,  'Unaouthorized')
+
+    @cherrypy.tools.accept(media='text/plain')
+    def DELETE(self, id):
+        db = cherrypy.request.db
+        user = cherrypy.session['user']
+        if user is None:
+            raise cherrypy.HTTPError(401, 'Unauthorized')
+        legal_ids = list(map(lambda x: int(x.household_id), user.accessrights))
+        if int(id) in legal_ids:
+            household = db.query(Household).filter(Household.id == id).one()
+            db.delete(household)
 
 if __name__ == '__main__':
     from lib.plugin.saplugin import SAEnginePlugin
