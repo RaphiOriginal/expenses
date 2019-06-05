34173f2d3d8b2ecc95c86b9aded843e276a8647e
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-06-05 22:42:17 +0200

    creating receipt model

---
+++ lib/model/receipt.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, Date, Decimal, ForeignKey
+from sqlalchemy.orm import relationship
+from base import Base
+
+class Receipt(Base):
+    __tablename__ = 'receipt'
+
+    id = Column(Integer, primary_key=True)
+
+    amount = Column(Decimal(7,2))
+    date = Column(Date)
+    member_id = Column(Integer, ForeignKey('member.id'))
+
+    member = relationship("Member", uselist=False, back_populates='receipts')
+
+    def __repr__(self):
+        return "<Receipt(amount='%s', date='%s', member_id='%s')>" % (self.amount, self.date, self.member_id)
