ff7422f493b1a2621f4131beba8d74e5fc1b764c
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-06-06 23:12:15 +0200

    use password and dbtool im householdservice

--- lib/tool/databasetool.py
+++ lib/tool/databasetool.py
@@ -1,5 +1,6 @@
 import cherrypy
 from lib.model.user import User
+from lib.model.household import Household
 
 def db():
     return cherrypy.request.db
@@ -13,5 +14,11 @@ def delete(object):
 def add(object):
     db().add(object)
 
+def commit():
+    db().commit()
+
 def findUsersByEmail(email):
     return db().query(User).filter_by(email=email).all()
+
+def findHouseholdById(id):
+    return db().query(Household).filter(Household.id == id).one()
