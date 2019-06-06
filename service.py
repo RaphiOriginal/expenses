06a7c34c021049ff04694d85d091c3fe95edb137
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-06-06 22:02:57 +0200

    extract member service class into own file

--- service.py
+++ service.py
@@ -1,9 +1,9 @@
 import cherrypy
 from lib.model.user import User
 from lib.model.household import Household
-from lib.model.member import Member
 from lib.model.accessright import AccessRight
 from lib.service.receiptservice import ReceiptService
+from lib.service.memberservice import MemberService
 import hashlib, uuid
 from datetime import datetime
 from cherrypy.lib import auth_basic
@@ -143,71 +143,6 @@ class HouseholdService(object):
         if int(id) in legal_ids:
             household = db.query(Household).filter(Household.id == id).one()
             db.delete(household)
-
-@cherrypy.expose
-class MemberService(object):
-
-    @cherrypy.tools.accept(media='text/plain')
-    def POST(self, name, household_id):
-        db = cherrypy.request.db
-        user = cherrypy.session['user']
-        if user is None:
-            raise cherrypy.HTTPError(401, 'Unauthorized')
-        legal_ids = list(map(lambda x: int(x.household_id), user.accessrights))
-        if int(household_id) in legal_ids:
-            member = Member(name = name, household_id = int(household_id))
-            db.add(member)
-        else:
-            raise cherrypy.HTTPError(401, 'Unauthorized')
-
-    @cherrypy.tools.accept(media='text/plain')
-    def GET(self, id=None, household_id=None):
-        db = cherrypy.request.db
-        user = cherrypy.session['user']
-        if user is None:
-            raise cherrypy.HTTPError(401, 'Unauthorized')
-        legal_ids = list(map(lambda x: int(x.household_id), user.accessrights))
-        if id is None and household_id is None:
-            members = db.query(Member).filter(Member.household_id.in_(legal_ids)).all()
-            return str(members)
-        elif id is None and int(household_id) in legal_ids:
-            members = db.query(Member).filter(Member.household_id == household_id).all()
-            return str(members)
-        else:
-            member = db.query(Member).filter(Member.id == id).one()
-            h_id = member.household.id
-            if h_id in legal_ids:
-                return str(member)
-            else:
-                raise cherrypy.HTTPError(401, 'Unauthorized')
-
-    @cherrypy.tools.accept(media='text/plain')
-    def PUT(self, id, name):
-        db = cherrypy.request.db
-        user = cherrypy.session['user']
-        if user is None:
-            raise cherrypy.HTTPError(401, 'Unauthorized')
-        legal_ids = list(map(lambda x: int(x.household_id), user.accessrights))
-        member = db.query(Member).filter(Member.id == id).one()
-        h_id = member.household.id
-        if h_id in legal_ids:
-            member.name = name
-        else:
-            raise cherrypy.HTTPError(401, 'Unauthorized')
-
-    @cherrypy.tools.accept(media='text/plain')
-    def DELETE(self, id):
-        db = cherrypy.request.db
-        user = cherrypy.session['user']
-        if user is None:
-            raise cherrypy.HTTPError(401, 'Unauthorized')
-        legal_ids = list(map(lambda x: int(x.household_id), user.accessrights))
-        member = db.query(Member).filter(Member.id == id).one()
-        h_id = member.household_id
-        if h_id in legal_ids:
-            db.delete(member)
-        else:
-            raise cherrypy.HTTPError(401, 'Unauthorized')
 
 if __name__ == '__main__':
     from lib.plugin.saplugin import SAEnginePlugin
